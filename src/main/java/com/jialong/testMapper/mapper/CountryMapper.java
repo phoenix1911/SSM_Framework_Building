e22a2a38bf6618b54e02c1d6d05a4dca9998d7a8
Jialong.Tian21882 <thisistjl@outlook.com>
2019-03-15 16:02:09 +0800

    .

---
+++ src/main/java/com/jialong/testMapper/mapper/CountryMapper.java
@@ -0,0 +1,7 @@
+package com.jialong.testMapper.mapper;
+
+import com.jialong.testMapper.bean.Country;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CountryMapper extends Mapper<Country> {
+}
---
+++ src/main/java/com/jialong/testMapper/mapper/StudentMapper.java
@@ -0,0 +1,7 @@
+package com.jialong.testMapper.mapper;
+
+import com.jialong.testMapper.bean.Student;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StudentMapper extends Mapper<Student> {
+}
